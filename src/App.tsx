8b3dff0783c2e546df90cdebe5f5ebd3f00d1697
Vladyslav Kutsevolov <kytsevolov@gmail.com>
2021-04-28 10:41:53 -0600

    created nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Movies from './components/Movies';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <Movies />
     </div>
   );
